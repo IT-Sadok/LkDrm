8b2a49224a4cacfffd6dc9dc318af81588e29fa6
Vitalii Kiral <vitalii.kiral@comap-control.com>
2026-05-19 11:22:45 +0200

    Fix bad comments

--- ConsoleLibrary/Interfaces/IBookRepository.cs
+++ ConsoleLibrary/Interfaces/IBookRepository.cs
@@ -3,18 +3,18 @@
 namespace ConsoleLibrary.Interfaces;
 
 /// <summary>
-/// Defines the contract for loading and saving the library data in JSON format.
+/// Defines the contract for loading and saving the library data.
 /// </summary>
 public interface IBookRepository
 {
     /// <summary>
-    /// Loads the library collection from the JSON file.
+    /// Loads the library collection from the file.
     /// </summary>
     /// <returns>A list of books, or an empty list if no file exists.</returns>
     List<BookInfo> Load();
 
     /// <summary>
-    /// Saves the library collection to the JSON file.
+    /// Saves the library collection to the file.
     /// </summary>
     /// <param name="library">The list of books to persist.</param>
     void Save(List<BookInfo> library);
